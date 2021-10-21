3a085beea0e63ef4953e054d9fe2e7204ffc76e5
lucasaleixo <lap15.ufpr@gmail.com>
2021-10-21 16:44:30 -0300

    displayed recipe to user

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,10 +31,15 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return; 
+
     // Loading Recipe
     renderSpinner(recipeContainer);
 
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
 
     const data = await res.json();
 
@@ -152,3 +157,5 @@ const showRecipe = async function() {
 };
 
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
