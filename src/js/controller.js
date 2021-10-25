e207c5fb5486c8e3465a1c92e755151cbf480abb
lucasaleixo <lap15.ufpr@gmail.com>
2021-10-25 14:04:50 -0300

    added searching

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,9 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function() {
   try {
@@ -30,6 +25,23 @@ const controlRecipes = async function() {
   }
 };
 
+const controlSearchResults = async function() {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+}; controlSearchResults();
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHadlerSearch(controlSearchResults);
 }; init();
