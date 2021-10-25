0bc0ea630f0318cccd7df2276598536e253a9741
lucasaleixo <lap15.ufpr@gmail.com>
2021-10-25 16:46:00 -0300

    updated recipe servings dynamically

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,7 +27,6 @@ const controlRecipes = async function() {
     // Rendering recipe
     recipeView.render(model.state.recipe);
 
-
   } catch (err) {
     recipeView.renderError();
   }
@@ -62,8 +61,17 @@ const controlPagination = function(goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function(newServings) {
+  // Update servings and ingredients
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHadlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 }; init();
